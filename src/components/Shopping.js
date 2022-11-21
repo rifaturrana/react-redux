b1b4fa066ad10e994e950b318865c423abe6da4b
rifaturrana <smrifaturrana@gmail.com>
2022-11-21 06:57:01 +0600

    firebase done

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Auth from "./Auth";
 import Layout from "./Layout";
+import { Snackbar } from "@mui/material";
 
 const Shopping = () => {
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  const cart = useSelector((state) => state.cart);
+
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://react-redux-4e09e-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+    };
+    sendRequest();
+  }, [cart]);
   return (
     <div>
+      <Snackbar type="success" message={"This is a dummy message"} />
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
